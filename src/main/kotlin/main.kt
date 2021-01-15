b88ebb6373368836a01044907bf10037b8ab5790
Mr.NiceGuy <dekel.honen@gmail.com>
2021-01-15 15:43:11 +0200

    Finished first version of the bot

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,25 @@
-fun main() {
-    println("Praise Joko!")
+import com.jessecorbett.diskord.dsl.bot
+import com.jessecorbett.diskord.dsl.command
+import com.jessecorbett.diskord.dsl.commands
+import configurations.DiscordConfigurations
+import handlers.PalawaBotoHandler
+
+    suspend fun main() {
+    val botToken = DiscordConfigurations.properties.getProperty(
+        DiscordConfigurations.PropertyKeys.DISCORD_BOT_TOKEN
+    )
+
+    val handler = PalawaBotoHandler()
+
+    bot(botToken) {
+        commands("") {
+            command("praise joko") {
+                reply(handler.generateRandomImageRespose())
+                reply(handler.generateRandomTextRespose())
+            }
+            command("!help") {
+                reply(handler.generateHelpMessage())
+            }
+        }
+    }
 }
